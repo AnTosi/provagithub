d76a0174d1d8d97b380bd10758cc512a6345ece0
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-09 17:04:24 +0100

    Title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ciccia
                 </div>
 
                 <div class="links">
